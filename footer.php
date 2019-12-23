90ef28b5fbdd706f761bac0be8f487612b758375
TsigeredaBerhane <39336200+TsigeredaBerhane@users.noreply.github.com>
2019-12-23 14:16:51 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+<footer>
+    <div class="container">
+    <?php 
+    	wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ;
+        wp_footer();
+        
+
+        ?>
+        
+        <?php echo('')?>
+    </div>
+    
+
+</footer>
+</body>
+</html>
