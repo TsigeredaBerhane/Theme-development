44b56e8cd9050af147013a3b352caf2059d15f07
TsigeredaBerhane <39336200+TsigeredaBerhane@users.noreply.github.com>
2020-01-27 11:27:12 -0800

    Add files via upload

--- page.php
+++ page.php
@@ -1,14 +1,47 @@
-<?php get_header() ?>
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bellablog
+ */
 
+get_header();
+?>
 <div class="container">
+  <div class="row">
+    <div class="col-sm-8">
 
-<h1><?php the_title()?></h1>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-<?php if(have_posts()): while(have_posts()): the_post(); 
-    the_content();
+		<?php
+		while ( have_posts() ) :
+			the_post();
 
-endwhile; endif?>
+			get_template_part( 'template-parts/content', 'page' );
 
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+ </div>
+    <div class="col-sm-4">
+    <?php get_sidebar(); ?>
+    </div>
+  </div>
 </div>
-
-<?php get_footer() ?>
+<?php get_footer(); ?>
