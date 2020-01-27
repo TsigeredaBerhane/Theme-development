41be795bf03b19a16fa8451f7f17167867df2187
TsigeredaBerhane <39336200+TsigeredaBerhane@users.noreply.github.com>
2020-01-27 11:34:58 -0800

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,17 +1,60 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package bellablog
+ */
 
-<footer>
-    <div class="container">
-    <?php 
-    	wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ;
-        wp_footer();
-        
+?>
 
+	</div><!-- #content -->
+
+	
+
+	<footer id="colophon" class="site-footer">
+
+
+	<nav id="footer-navigation"  class="navbar navbar-expand-md navbar-dark top">
+    <a class="navbar-brand" href="#">
+        <?php bloginfo('name'); ?>
+    </a>
+    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bellablog' ); ?></button>
+
+
+<?php
+            wp_nav_menu([
+            'menu'            => 'primary-menu',
+            'theme_location'  => 'menu-1',
+            'container'       => 'div',
+            'container_id'    => 'navbarCollapse',
+            'container_class' => 'collapse navbar-collapse',
+            'menu_id'         => false,
+            'menu_class'      => 'navbar-nav mr-auto',
+            'depth'           => 0,
+            'fallback_cb'     => 'functions::fallback',
+            'walker'          => new bella_navbar()
+            ]);
         ?>
-        
-        <?php echo('')?>
-    </div>
-    
 
-</footer>
+		</nav>
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bellablog' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'bellablog' ), 'Bella' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+			
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
 </body>
 </html>
